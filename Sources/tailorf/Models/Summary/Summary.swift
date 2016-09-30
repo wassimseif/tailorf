dbe10af2acf2e6a8995eea2367e3b7594f4ad610
Wassim  Seifeddine <wassim_seifeddine95@live.com>
2016-09-30 16:01:57 +0300

    Added parsing for summary

---
+++ Sources/tailorf/Models/Summary/Summary.swift
@@ -0,0 +1,37 @@
+//
+//  Summary.swift
+//  tailorf
+//
+//  Created by Wassim on 9/30/16.
+//
+//
+
+import Foundation
+
+class Summary {
+    var violationsCount : Double!
+    var warningsCount : Double!
+    var analyzedCount : Double!
+    var errorsCount : Double!
+    var skippedCount : Double!
+    
+    init(violationsCount : Double, warningsCount : Double, analyzedCount : Double , errorsCount : Double, skippedCount : Double) {
+        self.violationsCount = violationsCount
+        self.warningsCount = warningsCount
+        self.analyzedCount = analyzedCount
+        self.errorsCount = errorsCount
+        self.skippedCount = skippedCount
+        
+    }
+    class func initFrom(Object object : AnyObject ) -> Summary?{
+        guard let violationsCount = object["violations"] as? Double,
+              let warningsCount = object["warnings"] as? Double,
+              let analyzedCount = object["analyzed"] as? Double,
+            let errorsCount = object["errors"] as? Double,
+            let skippedCount = object["skipped"] as? Double else {
+                return nil
+        }
+        return Summary(violationsCount: violationsCount, warningsCount: warningsCount, analyzedCount: analyzedCount, errorsCount: errorsCount, skippedCount: skippedCount)
+        
+    }
+}
--- Sources/tailorf/main.swift
+++ Sources/tailorf/main.swift
@@ -1,7 +1,7 @@
-import Foundation
-
-
-func parseJSON (withData data: Data ) -> [String : AnyObject]?{
+ import Foundation
+ 
+ 
+ func parseJSON (withData data: Data ) -> [String : AnyObject]?{
     do {
         let json = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String:AnyObject]
         return json
@@ -10,15 +10,18 @@ func parseJSON (withData data: Data ) -> [String : AnyObject]?{
         return nil
     }
     
-}
-
-
-
-/// Gets a hand of the standard input
-let stdin = FileHandle.standardInput
-/// This will hault the program until it gets it's input
-let data  = stdin.readDataToEndOfFile()
-let json = parseJSON(withData: data)
-print(json!["summary"])
-
-
+ }
+ 
+ 
+ 
+ /// Gets a hand of the standard input
+ let stdin = FileHandle.standardInput
+ /// This will hault the program until it gets it's input
+ let data  = stdin.readDataToEndOfFile()
+ let json = parseJSON(withData: data)
+ 
+ 
+ let summary = Summary.initFrom(Object: json!["summary"]!)
+ 
+ print(summary!.analyzedCount)
+ 
