9085b5979bc26eca78008fff33abfd0b7a5c6b77
Wassim  Seifeddine <wassim_seifeddine95@live.com>
2016-09-30 16:08:05 +0300

    Added SwiftyBeaver

--- Package.swift
+++ Package.swift
@@ -4,6 +4,6 @@ let package = Package(
     name: "tailorf",
     targets: [],
     dependencies: [
-          
+          .Package(url: "https://github.com/SwiftyBeaver/SwiftyBeaver.git", majorVersion: 1)
         ]
 )
--- Sources/tailorf/main.swift
+++ Sources/tailorf/main.swift
@@ -1,5 +1,20 @@
  import Foundation
- 
+ import SwiftyBeaver
+  let log = SwiftyBeaver.self
+ func setupSwiftyBeaver(){
+
+    let console = ConsoleDestination()  // log to Xcode Console
+    let file = FileDestination()  // log to default swiftybeaver.log file
+    let cloud = SBPlatformDestination(appID: "foo", appSecret: "bar", encryptionKey: "123") // to cloud
+    
+    // use custom format and set console output to short time, log level & message
+    console.format = "$DHH:mm:ss$d $L $M"
+    
+    // add the destinations to SwiftyBeaver
+    log.addDestination(console)
+    log.addDestination(file)
+    log.addDestination(cloud)
+ }
  
  func parseJSON (withData data: Data ) -> [String : AnyObject]?{
     do {
@@ -13,13 +28,13 @@
  }
  
  
- 
+ setupSwiftyBeaver()
+ log.verbose("testing verbose")
  /// Gets a hand of the standard input
  let stdin = FileHandle.standardInput
  /// This will hault the program until it gets it's input
  let data  = stdin.readDataToEndOfFile()
  let json = parseJSON(withData: data)
- 
  
  let summary = Summary.initFrom(Object: json!["summary"]!)
  
