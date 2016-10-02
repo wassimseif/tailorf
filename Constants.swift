cd7e54f6adceda7f27551d4e23b32f42c5403a00
Wassim S Seifeddine <wassim_seifeddine95@live.com>
2016-10-03 00:02:27 +0300

    Added the constants class

---
+++ Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  tailorf
+//
+//  Created by Wassim Seifeddine on 10/2/16.
+//
+//
+
+import Foundation
+class Constants {
+
+    static let dateFormat = "EEEE MMM-dd-yyy,hh:m a"
+}
--- ReportHandler.swift
+++ ReportHandler.swift
@@ -10,14 +10,25 @@ import Foundation
 
 
 class ReportHandler {
-    var path : String?
+    var path : String!
     var files : [File]!
+    var reportName : String!
     init(withFiles files : [File]) {
         self.files = files
-     }
-    
+        self.reportName = getReportName()
+        generateReport(atPath: "/Users/wassimseifeddine/Desktop")
+        
+    }
+    private func getReportName() -> String {
+        let todaysDate : Date = Date()
+        let dateFormatter:DateFormatter = DateFormatter()
+        dateFormatter.dateFormat = Constants.dateFormat
+        let todayString:String = dateFormatter.string(from: todaysDate)
+        return todayString + ".html"
+    }
     func generateReport(atPath path : String){
         self.path = path
+        self.path = self.path + "/" + self.reportName!
         
     }
 }
